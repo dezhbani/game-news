5902fffc4325d33ef1eef8bd120f4575836d38b0
matin dezhbani <dezhbanimatin@gmail.com>
2025-03-02 16:40:11 +0330

    add products of shop to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,8 @@ import slider1 from '../assets/slider1.jpg'
 import slider2 from '../assets/slider2.png'
 import slider3 from '../assets/slider3.png'
 import slider4 from '../assets/slider4.png'
-import slider5 from '../assets/slider5.png'
 import slider6 from '../assets/slider6.png'
+import Products from '../components/Products';
 
 const Home = () => {
     const sliderData = [
@@ -16,17 +16,17 @@ const Home = () => {
         slider3,
         slider1,
         slider4,
-        slider5,
         slider6,
         slider4,
     ]
     return (
         <div className="h-full font-iranYekan flex flex-col bg-cover bg-center bg-no-repeat homeBg relative  dir-rtl">
-            <div className="absolute z-10 inset-0 bg-black opacity-50"></div>
+            <div className="absolute z-10 inset-0 bg-black bg-opacity-50 backdrop-blur-[114px] opacity-80"></div>
             <div className="relative z-10 flex flex-col flex-grow mx-24 my-5">
                 <div className="w-full">
                     <Navbar />
                     <Slider data={sliderData} />
+                    <Products />
                 </div>
             </div>
             <div className="w-full mt-auto z-20">
