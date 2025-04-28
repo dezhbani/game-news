26d27155f8b6a7d93e9e4b0085d9615d8583b9ef
matin dezhbani <dezhbanimatin@gmail.com>
2025-04-28 14:25:05 +0330

    feat: routing pages & add loading animation styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { ToastContainer } from "react-toastify";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <ToastContainer />
+    </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import UserContext from './contexts/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.baseURL = 'http://localhost:2000';
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <UserContext>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </UserContext>
 );
 
