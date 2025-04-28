7203fecf07ba5607fe98c6912e6bce2920812df8
matin dezhbani <dezhbanimatin@gmail.com>
2025-04-28 12:16:19 +0330

    feat: add functions for fetching data from API endpoints

---
+++ src/services/home.service.js
@@ -0,0 +1,59 @@
+import axios from "axios"
+
+const getSlider = async () => {
+    try {
+        const result = await axios.get('/sliders')
+        return result.data
+    } catch (error) {
+        return null
+    }
+}
+const getProducts = async () => {
+    try {
+        const result = await axios.get('/products')
+        return result.data
+    } catch (error) {
+        return null
+    }
+}
+const getArticles = async () => {
+    try {
+        const result = await axios.get('/articles')
+        return result.data
+    } catch (error) {
+        return null
+    }
+}
+const getReviews = async () => {
+    try {
+        const result = await axios.get('/reviews')
+        return result.data
+    } catch (error) {
+        return null
+    }
+}
+const getTrailers = async () => {
+    try {
+        const result = await axios.get('/trailers')
+        return result.data
+    } catch (error) {
+        return null
+    }
+}
+const getTrailerVideo = async () => {
+    try {
+        const result = await axios.get('/trailerVideo')
+        return result.data
+    } catch (error) {
+        return null
+    }
+}
+
+export {
+    getSlider,
+    getProducts,
+    getArticles,
+    getReviews,
+    getTrailers,
+    getTrailerVideo
+}
