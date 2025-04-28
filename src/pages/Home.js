44517ee12f4588e5d4b0df16b535e5178af4ddcc
matin dezhbani <dezhbanimatin@gmail.com>
2025-04-28 12:22:31 +0330

    feat: fetch data from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,102 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import React, { useEffect, useState } from 'react';
+import Navbar from '../components/public/Navbar';
+import Footer from '../components/home/Footer';
 import Slider from '../modules/Slider';
 
 // Images
-import slider1 from '../assets/slider/slide1.jpg'
-import slider2 from '../assets/slider/slide2.png'
-import slider3 from '../assets/slider/slide3.png'
-import slider4 from '../assets/slider/slide4.png'
-import slider6 from '../assets/slider/slide6.png'
+import Logo from '../assets/Logo.svg'
 
 // Icons
-import Products from '../components/Products';
-import Articles from '../components/Articles';
-import Reviews from '../components/Reviews';
-import TrailerVideo from '../components/TrailerVideo';
-import Trailers from '../components/Trailers';
+import Products from '../components/home/Products';
+import Articles from '../components/home/Articles';
+import Reviews from '../components/home/Reviews';
+import TrailerVideo from '../components/home/TrailerVideo';
+import Trailers from '../components/home/Trailers';
+import { getArticles, getProducts, getReviews, getSlider, getTrailers, getTrailerVideo } from '../services/home.service';
 
 const Home = () => {
-    const sliderData = [
-        slider2, 
-        slider3,
-        slider1,
-        slider4,
-        slider6,
-        slider4,
-    ]
+    const [data, setData] = useState({
+        sliders: null,
+        products: null,
+        articles: null,
+        reviews: null,
+        trailers: null,
+        trailerVideo: null
+    })
+    const [loading, setLoading] = useState(true)
+    const setHomeData = (key, value) => setData(prevData => ({ ...prevData, [key]: value }))
+    const getSlidersData = async () => {
+        const slidersData = await getSlider()
+        setHomeData("sliders", slidersData[0])
+    }
+    const getProductsData = async () => {
+        const productsData = await getProducts()
+        setHomeData("products", productsData)
+    }
+    const getArticlesData = async () => {
+        const articletsData = await getArticles()
+        setHomeData("articles", articletsData)
+    }
+    const getReviewsData = async () => {
+        const reviewsData = await getReviews()
+        setHomeData("reviews", reviewsData)
+    }
+    const getTrailerVideoData = async () => {
+        const trailerVideoData = await getTrailerVideo()
+        
+        setHomeData("trailerVideo", trailerVideoData[0]) 
+    }
+    const getTrailersData = async () => {
+        const trailersData = await getTrailers()
+        setHomeData("trailers", trailersData)
+    }
+    const handleLoading = () => {
+        const result = Object.values(data).filter(value => !value)
+        console.log(result);
+        !result.includes(null) && setLoading(false)
+    }
+    useEffect(() => {
+        setTimeout(() => {
+            handleLoading()
+        }, 5000);
+    }, [data])
+    useEffect(() => {
+        getSlidersData()
+        getProductsData()
+        getArticlesData()
+        getReviewsData()
+        getTrailerVideoData()
+        getTrailersData()
+    }, [])
+    
+    if (loading) return <div className='flex justify-center items-center w-screen h-screen bg-dark-gray'>
+        <img className='animate-pulse h-12' src={Logo} />
+        <div class="card">
+            <div class="loader">
+                <p>Loading</p>
+                <div class="words">
+                    <span class="word">games</span>
+                    <span class="word">trailers</span>
+                    <span class="word">news</span>
+                    <span class="word">reviews</span>
+                    <span class="word">guides</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
     return (
         <div className="h-full font-iranYekan flex flex-col bg-cover z-0 bg-center bg-no-repeat homeBg relative  dir-rtl">
             <div className="absolute z-10 inset-0 bg-dark-gray bg-opacity-90 backdrop-blur-[114px] opacity-80"></div>
             <div className="relative z-10 flex flex-col flex-grow mx-24 my-5">
                 <div className="w-full">
                     <Navbar />
-                    <Slider data={sliderData} />
-                    <Products />
-                    <Articles />
-                    <Reviews />
-                    <TrailerVideo />
-                    <Trailers />
+                    <Slider data={data.sliders} />
+                    <Products data={data.products} />
+                    <Articles data={data.articles} />
+                    <Reviews data={data.reviews} />
+                    <TrailerVideo data={data.trailerVideo} />
+                    <Trailers data={data.trailers} />
                 </div>
             </div>
             <div className="w-full mt-auto z-20">
