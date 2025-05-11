60f62fbb2cfade7ad3b2af20475380349197c41f
matin dezhbani <dezhbanimatin@gmail.com>
2025-05-12 01:37:29 +0330

    feat: add navbar & background component to pages

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,18 +2,18 @@ import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { ProfileContext } from '../contexts/UserContext';
 import SignupForm from '../components/auth/SignupForm';
+import Background from '../components/public/Background';
+import Navbar from '../components/public/Navbar';
 
 const Signup = () => {
-    const user = useContext(ProfileContext)
-    
+    const { user } = useContext(ProfileContext)
+
     if (user) return <Navigate replace to="/" />
     return (
-        <div className="min-h-screen font-iranYekan flex flex-col bg-cover z-0 bg-center bg-no-repeat homeBg relative dir-rtl">
-            <div className="absolute z-10 inset-0 bg-dark-gray bg-opacity-90 opacity-80"></div>
-            <div className="relative z-10 flex flex-col flex-grow mx-24 my-5 items-center justify-center">
-                <SignupForm />
-            </div>
-        </div>
+        <Background>
+            <Navbar logoVisiblity={false} />
+            <SignupForm />
+        </Background>
     );
 };
 
