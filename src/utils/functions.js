a74e69a5c9d8f6dff3020f3419a4ccbbc1a2fffd
matin dezhbani <dezhbanimatin@gmail.com>
2025-05-12 01:54:28 +0330

    style: responsive custome toast

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,7 +1,7 @@
 import { toast } from "react-toastify";
 
 const textMaxLength = (text="", max = 110) => {
-    if (text.length > max) return text.slice(0, max) + "..."
+    if (text.length > max) return text.slice(0, max - 5) + " ..."
     else return text
 }
 
@@ -15,7 +15,7 @@ const customToast = (type, message) => {
         draggable: true,
         progress: false,
         closeButton: false,
-        className: "font-iranYekan dir-rtl shadow-[#F0F3F61A] shadow-md w-fit flex justify-center bg-dark-gray text-white rounded-2xl font-base px-5 py-0"
+        className: "font-iranSans dir-rtl shadow-[#F0F3F61A] shadow-md w-fit flex justify-center bg-dark-gray text-white rounded-2xl text-sm md:text-base max-md:mt-2 px-3 py-0"
     });
 };
 
