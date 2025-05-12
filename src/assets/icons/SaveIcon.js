7dd7993c0841942feaa95668d83a8b2bcfde384e
matin dezhbani <dezhbanimatin@gmail.com>
2025-05-12 10:12:11 +0330

    feat: add new icons for hamburger menu

--- src/assets/icons/SaveIcon.js
+++ src/assets/icons/SaveIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SaveIcon = (props) => {
     return (
-        <svg {...props} xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" stroke-width="2.5" stroke="currentColor">
+        <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"></path>
         </svg>
     );
---
+++ src/assets/icons/ShareIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShareIcon = (props) => {
+    return (
+        <svg {...props} width="31" height="30" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M25.75 14.9998L17 7.49976V12.541C12.2738 12.541 7 14.3748 7 22.4998C9.46125 18.5148 13.25 17.4998 17 17.4998V22.4998L25.75 14.9998Z" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        
+    );
+};
+
+export default ShareIcon;
