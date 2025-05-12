aa35d13d203d2d47eefd9abbeeac05cdbd54aded
matin dezhbani <dezhbanimatin@gmail.com>
2025-05-12 10:15:35 +0330

    feat: install and customize 'react-loading-skeleton'

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { SkeletonTheme } from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css';
 
 function App() {
   return (
     <>
+      <SkeletonTheme baseColor="#2e323d" highlightColor="#3c4155">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
       <ToastContainer />
+    </SkeletonTheme >
     </>
   )
 }
