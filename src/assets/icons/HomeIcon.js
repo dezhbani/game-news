53177e00ef09b9128f5dadd34e43f50ee78e059f
matin dezhbani <dezhbanimatin@gmail.com>
2025-05-19 18:00:23 +0330

    fix: fix console warnings

--- src/assets/icons/HomeIcon.js
+++ src/assets/icons/HomeIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomeIcon = (props) => {
     return (
         <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19 17" width="20">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="m.867 5.167 8.11-4.055a.5.5 0 0 1 .446 0l8.11 4.055m-1.666 2.5v6.666A1.666 1.666 0 0 1 14.2 16h-10a1.667 1.667 0 0 1-1.667-1.667V7.667"></path>
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="m.867 5.167 8.11-4.055a.5.5 0 0 1 .446 0l8.11 4.055m-1.666 2.5v6.666A1.666 1.666 0 0 1 14.2 16h-10a1.667 1.667 0 0 1-1.667-1.667V7.667"></path>
         </svg>
     );
 };
--- src/assets/icons/LikeIcon.js
+++ src/assets/icons/LikeIcon.js
@@ -3,8 +3,8 @@ import React from 'react';
 const LikeIcon = (props) => {
     return (
         <svg {...props} width="31" height="30" viewBox="0 0 31 30" xmlns="http://www.w3.org/2000/svg">
-            <rect x="6.125" y="14.3748" width="3.75" height="10" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M12.375 24.3748H21C22.1715 24.3746 23.1858 23.5609 23.44 22.4173L24.8288 16.1673C24.9935 15.4262 24.8126 14.6505 24.3371 14.0587C23.8615 13.467 23.1429 13.1234 22.3837 13.1248H17.375V8.12476C17.375 6.74404 16.2557 5.62476 14.875 5.62476L12.375 13.1248" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+            <rect x="6.125" y="14.3748" width="3.75" height="10" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M12.375 24.3748H21C22.1715 24.3746 23.1858 23.5609 23.44 22.4173L24.8288 16.1673C24.9935 15.4262 24.8126 14.6505 24.3371 14.0587C23.8615 13.467 23.1429 13.1234 22.3837 13.1248H17.375V8.12476C17.375 6.74404 16.2557 5.62476 14.875 5.62476L12.375 13.1248" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     );
 };
